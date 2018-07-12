d0a7770411bd16d4478f556bfc324782b3d45fdd
la0wei <weitingjiu@gmail.com>
2018-07-12 22:44:25 +0800

    Revert "what's summary"
    This reverts commit 9f7502f0ae4cfbb5549147b23fa5546aa76b61c7.

--- tr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt update && apt-get install python-software-properties nano -y
-add-apt-repository ppa:transmissionbt/ppa -y
-apt update
-apt-get install transmission-cli transmission-common transmission-daemon
-
-service transmission-daemon start
-service transmission-daemon stop
