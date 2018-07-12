e9ea160d0ea8ac30c53947ccb3697e242ea5f3da
la0wei <weitingjiu@gmail.com>
2018-07-12 22:53:22 +0800

    auto confirm

--- tr.sh
+++ tr.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-apt update && apt-get install python-software-properties nano -y
+apt update && apt-get install -y python-software-properties nano
 add-apt-repository ppa:transmissionbt/ppa -y
 apt update
-apt-get install transmission-cli transmission-common transmission-daemon
+apt-get install -y transmission-cli transmission-common transmission-daemon
 
-service transmission-daemon start
 service transmission-daemon stop
